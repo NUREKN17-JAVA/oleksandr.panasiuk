28d37d688502f16f93d88d9d6857c2ded15a9d49
AleksandrPanasiuk <oleksandr.panasiuk@nure.ua>
2019-11-06 23:53:10 +0200

    create userdao test

---
+++ usermanagement/src/main/java/ua/nure/kn/panasiuk/domain/db/DaoFactoryTest.java
@@ -0,0 +1,15 @@
+package ua.nure.kn.panasiuk.domain.db;
+
+import junit.framework.TestCase;
+
+public class DaoFactoryTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testGetUserDao() {
+		fail("Not yet implemented");
+	}
+
+}
