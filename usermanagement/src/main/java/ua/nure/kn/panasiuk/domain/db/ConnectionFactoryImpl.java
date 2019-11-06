611c0b8b19e400907e19a9a8814a7c74fbd55bfa
AleksandrPanasiuk <oleksandr.panasiuk@nure.ua>
2019-11-06 21:54:39 +0200

    create connectionFactory and impl

---
+++ usermanagement/src/main/java/ua/nure/kn/panasiuk/domain/db/ConnectionFactoryImpl.java
@@ -0,0 +1,13 @@
+package ua.nure.kn.panasiuk.domain.db;
+
+import java.sql.Connection;
+
+public class ConnectionFactoryImpl implements ConnectionFactory {
+
+	@Override
+	public Connection createConnection() throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
