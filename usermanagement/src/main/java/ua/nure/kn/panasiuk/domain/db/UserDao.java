6c4efea5b5390fdec207e969e30bb55b60547aaa
AleksandrPanasiuk <oleksandr.panasiuk@nure.ua>
2019-12-04 12:29:03 +0200

    Add gui and fix some bugs

--- usermanagement/src/main/java/ua/nure/kn/panasiuk/domain/db/UserDao.java
+++ usermanagement/src/main/java/ua/nure/kn/panasiuk/domain/db/UserDao.java
@@ -6,50 +6,17 @@ import ua.nure.kn.panasiuk.domain.User;
 
 public interface UserDao {
 	
-	/**
-     * add new record to db with user
-     * @param user with null id
-     * @return modified user with auto generated id from DB
-     *
-     */
-    User create(User user) throws DatabaseException;
+	User create(User user) throws DatabaseException;
+	
+	void update(User user) throws DatabaseException;
 
-    /**
-     *
-     * @param id
-     * @return
-     * @throws DatabaseException
-     */
-    User find(long id) throws DatabaseException;
+    void delete(User user) throws DatabaseException;
 
-    /**
-     *
-     * @return
-     * @throws DatabaseException
-     */
-    Collection<User> getAll() throws DatabaseException;
+    User find(Long ID) throws DatabaseException;
 
-    /**
-     * all field
-     * @param user
-     * @throws DatabaseException
-     */
-    void update (User user) throws DatabaseException;
+    Collection <User> findAll() throws DatabaseException;
 
-    /**
-     *
-     * @param user
-     * @throws DatabaseException
-     */
-    void delete (User user) throws DatabaseException;
-
-    /**
-     *
-     * @param connectionFactory
-     */
     void setConnectionFactory(ConnectionFactory connectionFactory);
-    
-    Collection<User> findAll() throws DatabaseException;
 
 }
 	
