7807606c68861ea575429540be65f7beccb00d3c
AleksandrPanasiuk <oleksandr.panasiuk@nure.ua>
2019-12-19 00:14:06 +0200

    add function for test deleteServlet

--- usermanagement/src/test/java/ua/nure/kn/panasiuk/domain/web/DeleteServletTest.java
+++ usermanagement/src/test/java/ua/nure/kn/panasiuk/domain/web/DeleteServletTest.java
@@ -1,5 +1,23 @@
 package ua.nure.kn.panasiuk.domain.web;
 
-public class DeleteServletTest {
+import ua.nure.kn.panasiuk.domain.User;
 
+import java.util.Date;
+
+public class DeleteServletTest extends MockServletTestCase {
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        createServlet(DeleteServlet.class);
+
+    }
+
+    public void testDelete() {
+        User user = new User(new Long(1000), "John", "Doe", new Date());
+        getMockUserDao().expect("delete", user);
+        setSessionAttribute("user", user);
+        addRequestParameter("ok", "Ok");
+        doPost();
+    }
 }
