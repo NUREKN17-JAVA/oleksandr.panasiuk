62e9197297ecfb1380c11b3b011c83ec01053fa5
AleksandrPanasiuk <oleksandr.panasiuk@nure.ua>
2019-12-19 00:17:22 +0200

    add function for test MockServletCase

--- usermanagement/src/test/java/ua/nure/kn/panasiuk/domain/web/MockServletTestCase.java
+++ usermanagement/src/test/java/ua/nure/kn/panasiuk/domain/web/MockServletTestCase.java
@@ -1,5 +1,39 @@
 package ua.nure.kn.panasiuk.domain.web;
 
-public class MockServletTestCase {
+import java.util.Calendar;
+import java.util.Properties;
 
+import com.mockobjects.dynamic.Mock;
+import com.mockrunner.servlet.BasicServletTestCaseAdapter;
+
+import ua.nure.kn.panasiuk.domain.User;
+import ua.nure.kn.panasiuk.domain.db.DaoFactory;
+import ua.nure.kn.panasiuk.domain.db.MockDaoFactory;
+
+public abstract class MockServletTestCase extends BasicServletTestCaseAdapter {
+
+    private Mock mockUserDao;
+
+    protected Mock getMockUserDao() {
+        return mockUserDao;
+    }
+
+    protected void setMockUserDao(Mock mockUserDao) {
+        this.mockUserDao = mockUserDao;
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        Properties properties = new Properties();
+        properties.setProperty("dao.factory", MockDaoFactory.class.getName());
+        DaoFactory.init(properties);
+        setMockUserDao(((MockDaoFactory) DaoFactory.getInstance()).getMockUserDao());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        getMockUserDao().verify();
+        super.tearDown();
+    }
 }
