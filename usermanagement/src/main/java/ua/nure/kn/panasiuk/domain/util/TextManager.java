149e9189e20493fefa4808d6c05914561de451b4
AleksandrPanasiuk <oleksandr.panasiuk@nure.ua>
2019-12-25 14:54:23 +0200

    add function for text manager

--- usermanagement/src/main/java/ua/nure/kn/panasiuk/domain/util/TextManager.java
+++ usermanagement/src/main/java/ua/nure/kn/panasiuk/domain/util/TextManager.java
@@ -1,5 +1,28 @@
 package ua.nure.kn.panasiuk.domain.util;
 
-public class TextManager {
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
+/**
+ * A class that implements internationalization for user management application. Works with intern_text.properties file
+ */
+
+public class TextManager {
+	
+	private static final String BUNDLE_NAME = "intern_text";
+
+    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+    private TextManager() {
+
+    }
+
+    public static String getString(String key) {
+        try {
+            return BUNDLE.getString(key);
+        } catch (MissingResourceException e) {
+            return "!" + key + "!";
+        }
+    }
 }
