30a22f2e8e7aa5f2b97ca3e65b3e7bb7973675bd
AleksandrPanasiuk <oleksandr.panasiuk@nure.ua>
2019-12-25 14:44:53 +0200

    add function for search exception

--- usermanagement/src/main/java/ua/nure/kn/panasiuk/domain/agent/SearchException.java
+++ usermanagement/src/main/java/ua/nure/kn/panasiuk/domain/agent/SearchException.java
@@ -1,5 +1,26 @@
 package ua.nure.kn.panasiuk.domain.agent;
 
-public class SearchException {
+/**
+ * This exception marks excetions, that are connected to the execution of a search agent
+ */
+public class SearchException extends Exception {
 
+    public SearchException() {
+    }
+
+    public SearchException(String message) {
+        super(message);
+    }
+
+    public SearchException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SearchException(Throwable cause) {
+        super(cause);
+    }
+
+    public SearchException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
